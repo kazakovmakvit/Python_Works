885af5b0304a55fa721d66ccfa1cb73ade48deb6
Maxim <kazakovmvmvm@gmail.com>
2022-09-26 21:03:39 +0300

    Task_2 remote

--- Python_Home_Work_1/Task_2.py
+++ Python_Home_Work_1/Task_2.py
@@ -1,1 +1,19 @@
-Не понимаю, как решить 
+"""" Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.  """
+
+#{Не уверен что верно!}
+
+
+xyz = ["X", "Y", "Z"]
+a = []
+for i in range(3):
+    a.append(input(f"Введите значение {xyz[i]}: "))
+
+left = not (xyz[0] or xyz[1] or xyz[2])
+right = not xyz[0] and not xyz[1] and not xyz[2]
+result = left == right
+
+
+if result == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
